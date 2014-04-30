6675a42d6366d774e50b89f3956f71aec63aa299
Seth McLaughlin <s@sethmcl.com>
2014-04-30 14:50:57 -0700

    Test fs on azure

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
+var fs   = require('fs');
+var path = require('path');
 
 http.createServer(function (request, response) {
-  response.end('Hello');
+  fs.createReadStream(path.resolve(__dirname, 'test.js')).pipe(response);
 }).listen(port);
